c4a2adc0bb7e21ecfaab23a3d290f3269d20bf6a
arsalan rahimi <years1373@yahoo.com>
2021-12-17 14:52:07 -0800

    logout added

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react"
+import axiosWithAuth from "./../utils/axiosWithAuth"
 
-const Logout = () => {        
-    return(<div></div>);
+const Logout = (props) => {
+  axiosWithAuth()
+    .post(`http://localhost:5000/api/logout`)
+    .then((res) => {
+      localStorage.removeItem("token")
+      props.history.push("/login")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+  return <div>Peace</div>
 }
 
-export default Logout;
+export default Logout
 
 // Task List
 // 1. On mount, execute a http request to the logout endpoint.
