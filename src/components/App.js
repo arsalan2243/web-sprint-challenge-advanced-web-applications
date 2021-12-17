100e9ed3814ce8494999bdb03456b2fbfd59ddaa
arsalan rahimi <years1373@yahoo.com>
2021-12-17 14:54:45 -0800

    article test finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import { Route } from "react-router-dom";
-import styled from 'styled-components';
+import React from "react"
+import { Route } from "react-router-dom"
+import PrivateRoute from "./PrivateRoute"
+import styled from "styled-components"
 
-import Header from './Header';
-import BloomHeader from './BloomHeader';
-import Login from './Login';
+import Header from "./Header"
+import BloomHeader from "./BloomHeader"
+import View from "./View"
+import Login from "./Login"
+import Logout from "./Logout"
 
 const App = () => {
   return (
     <AppContainer>
-      <BloomHeader/>
-      <Header/>
+      <BloomHeader />
+      <Header />
       <RouteContainer>
         <Route exact path="/">
-          <Login/>
-        </Route>          
+          <Login />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <PrivateRoute path="/view">
+          <View />
+        </PrivateRoute>
+        <PrivateRoute path="/logout" component={Logout} />
       </RouteContainer>
     </AppContainer>
   )
 }
 
-export default App;
+export default App
 
 //Task List
 //1. Create and import PrivateRoute component.
 //2. Create a Route for Login pointing to '/login.'
 //3. Create a PrivateRoute for View component point to '/view.'
 //4. Create a PrivateRoute for Logout component pointing to '/logout.'
-
 
 const AppContainer = styled.div`
   height: 100%;
